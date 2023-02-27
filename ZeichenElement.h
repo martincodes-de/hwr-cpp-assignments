79dfd7e6c3976144fa3df62840467d51f29327e0
Malte Eisermann <online@eisermann.xyz>
2023-02-27 09:28:32 +0100

    4.4 done

--- ZeichenElement.h
+++ ZeichenElement.h
@@ -4,10 +4,14 @@
 #include "Koordinate.h"
 
 class ZeichenElement {
-public:
+protected:
     explicit ZeichenElement(Koordinate insertPoint) : m_insertPoint(insertPoint) {};
+
     explicit ZeichenElement() : m_insertPoint() {};
 
+    //Der Konstruktor der Basisklasse sollte im protected-Bereich definiert werden, um den Zugriff von unerwünschten
+    // Klassen und Funktionen zu verhindern. Das hat zur Konsequenz, dass der Konstruktor nur von abgeleiteten Klassen
+    // aufgerufen werden kann.
     Koordinate getInsertPoint() const { return m_insertPoint; }
 
 private:
